e7f076261488609c0ec97016bf9e1e0d08d61d3e
spacemonkey71 <brian.e.buckley@att.net>
2018-03-29 17:13:11 -0700

    first commit

---
+++ main/java/delvingmod/Main.java
@@ -0,0 +1,30 @@
+package delvingmod;
+
+import org.apache.logging.log4j.Logger;
+
+import delvingmod.utils.Reference;
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = Reference.MODID, name = Reference.NAME, version = Reference.VERSION)
+public class Main
+{
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
---
+++ main/java/delvingmod/utils/Reference.java
@@ -0,0 +1,9 @@
+package delvingmod.utils;
+
+public class Reference {
+	
+	public static final String MODID = "delvingmod";
+    public static final String NAME = "Delving Mod";
+    public static final String VERSION = "0.1";
+
+}
